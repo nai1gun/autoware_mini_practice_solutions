7df541c88fe2498c7e7613ca4626d573a4abc988
Alex Korvyakov <djkp9ik@gmail.com>
2025-07-28 14:12:59 +0300

    Two publishers

--- nodes/publisher.py
+++ nodes/publisher.py
@@ -3,7 +3,8 @@ import rospy
 from std_msgs.msg import String
 
 rospy.init_node('publisher')
-rate = rospy.Rate(2)
+rateValue = rospy.get_param('~rate', 2)  # Default rate is 2 Hz
+rate = rospy.Rate(rateValue)
 pub = rospy.Publisher('/message', String, queue_size=10)
 
 while not rospy.is_shutdown():
