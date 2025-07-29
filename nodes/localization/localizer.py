da7a6db6e42d2188f112421531ecf701ee4fd130
Alex Korvyakov <djkp9ik@gmail.com>
2025-07-29 14:13:22 +0300

    Publish current_velocity

--- nodes/localization/localizer.py
+++ nodes/localization/localizer.py
@@ -35,12 +35,52 @@ class Localizer:
         self.transformer = Transformer.from_crs(self.crs_wgs84, self.crs_utm)
         self.origin_x, self.origin_y = self.transformer.transform(utm_origin_lat, utm_origin_lon)
 
+    # convert azimuth to yaw angle
+    def convert_azimuth_to_yaw(self, azimuth):
+        """
+        Converts azimuth to yaw. Azimuth is CW angle from the north. Yaw is CCW angle from the East.
+        :param azimuth: azimuth in radians
+        :return: yaw in radians
+        """
+        yaw = -azimuth + math.pi/2
+        # Clamp within 0 to 2 pi
+        if yaw > 2 * math.pi:
+            yaw = yaw - 2 * math.pi
+        elif yaw < 0:
+            yaw += 2 * math.pi
+
+        return yaw
+
     def transform_coordinates(self, msg):
         # print(msg.latitude, msg.longitude)
         transformed_x, transformed_y = self.transformer.transform(msg.latitude, msg.longitude)
         substracted_x = transformed_x - self.origin_x
         substracted_y = transformed_y - self.origin_y
-        print(f"x:{substracted_x} :{substracted_y}")
+        # print(f"x:{substracted_x} :{substracted_y}")
+        # publish current pose
+        current_pose_msg = PoseStamped()
+        current_pose_msg.header.stamp = msg.header.stamp
+        current_pose_msg.header.frame_id = "map"
+        current_pose_msg.pose.position.x = substracted_x
+        current_pose_msg.pose.position.y = substracted_y
+        current_pose_msg.pose.position.z = msg.height - self.undulation
+
+        # calculate azimuth correction
+        azimuth_correction = self.utm_projection.get_factors(msg.longitude, msg.latitude).meridian_convergence
+        
+        azimuth_rad = math.radians(msg.azimuth)
+        # print(f"Azimuth:{msg.azimuth}")
+        # print(f"Azimuth rad:{azimuth_rad}")
+        # print(f"Correction:{azimuth_correction}")
+        
+        yaw = self.convert_azimuth_to_yaw(azimuth_rad - azimuth_correction)
+
+        # Convert yaw to quaternion
+        x, y, z, w = quaternion_from_euler(0, 0, yaw)
+        orientation = Quaternion(x, y, z, w)
+
+        current_pose_msg.pose.orientation = orientation
+        self.current_pose_pub.publish(current_pose_msg)
 
     def run(self):
         rospy.spin()
