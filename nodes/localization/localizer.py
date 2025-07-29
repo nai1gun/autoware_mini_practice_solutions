970de95cdb908144636fad94fb116ef4edcaf3ea
Alex Korvyakov <djkp9ik@gmail.com>
2025-07-29 13:15:29 +0300

    Add localizer node

---
+++ nodes/localization/localizer.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import math
+import rospy
+
+from tf.transformations import quaternion_from_euler
+from tf2_ros import TransformBroadcaster
+from pyproj import CRS, Transformer, Proj
+
+from novatel_oem7_msgs.msg import INSPVA
+from geometry_msgs.msg import PoseStamped, TwistStamped, Quaternion, TransformStamped
+
+class Localizer:
+    def __init__(self):
+
+        # Parameters
+        self.undulation = rospy.get_param('undulation')
+        utm_origin_lat = rospy.get_param('utm_origin_lat')
+        utm_origin_lon = rospy.get_param('utm_origin_lon')
+
+        # Internal variables
+        self.crs_wgs84 = CRS.from_epsg(4326)
+        self.crs_utm = CRS.from_epsg(25835)
+        self.utm_projection = Proj(self.crs_utm)
+
+        # Subscribers
+        rospy.Subscriber('/novatel/oem7/inspva', INSPVA, self.transform_coordinates)
+
+        # Publishers
+        self.current_pose_pub = rospy.Publisher('current_pose', PoseStamped, queue_size=10)
+        self.current_velocity_pub = rospy.Publisher('current_velocity', TwistStamped, queue_size=10)
+        self.br = TransformBroadcaster()
+
+    def transform_coordinates(self, msg):
+        print(msg.latitude, msg.longitude)
+
+    def run(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    rospy.init_node('localizer')
+    node = Localizer()
+    node.run()
