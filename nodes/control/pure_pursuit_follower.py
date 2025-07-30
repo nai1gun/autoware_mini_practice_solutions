82eca5b8067d7ecbf54ae2cda5603ddb8262215d
Alex Korvyakov <djkp9ik@gmail.com>
2025-07-30 14:45:31 +0300

    Dynamic linear velocity

--- nodes/control/pure_pursuit_follower.py
+++ nodes/control/pure_pursuit_follower.py
@@ -8,6 +8,7 @@ from autoware_mini.msg import VehicleCmd
 from shapely.geometry import LineString, Point
 from shapely import prepare, distance
 from tf.transformations import euler_from_quaternion
+from scipy.interpolate import interp1d
 
 class PurePursuitFollower:
     def __init__(self):
@@ -16,6 +17,7 @@ class PurePursuitFollower:
         self.path_linstring = None
         self.lookahead_distance = rospy.get_param('~lookahead_distance', 1.0)
         self.wheel_base = rospy.get_param('/vehicle/wheel_base', 3.0)
+        self.distance_to_velocity_interpolator = None
         print(f"Lookahead distance: {self.lookahead_distance} m, Wheel base: {self.wheel_base} m")
 
         # Publishers
@@ -33,6 +35,18 @@ class PurePursuitFollower:
         # prepare path - creates spatial tree, making the spatial queries more efficient
         prepare(path_linestring)
         self.path_linstring = path_linestring
+
+        # Create a distance-to-velocity interpolator for the path
+        # collect waypoint x and y coordinates
+        waypoints_xy = np.array([(w.position.x, w.position.y) for w in msg.waypoints])
+        # Calculate distances between points
+        distances = np.cumsum(np.sqrt(np.sum(np.diff(waypoints_xy, axis=0)**2, axis=1)))
+        # add 0 distance in the beginning
+        distances = np.insert(distances, 0, 0)
+        # Extract velocity values at waypoints
+        velocities = np.array([w.speed for w in msg.waypoints])
+        # Create an interpolator for distance to velocity
+        self.distance_to_velocity_interpolator = interp1d(distances, velocities, kind='linear', bounds_error=False, fill_value=0.0)
 
     def current_pose_callback(self, msg):
         current_pose = Point([msg.pose.position.x, msg.pose.position.y])
@@ -60,10 +74,18 @@ class PurePursuitFollower:
 
         steering_angle = np.arctan2(2 * self.wheel_base * np.sin(lookahead_heading - heading), dynamic_lookahead_distance)
 
+        # Calculate the velocity based on the distance from the path start
+        linear_velocity = 0.0
+        if self.distance_to_velocity_interpolator is not None:
+            linear_velocity = self.distance_to_velocity_interpolator(d_ego_from_path_start)
+
+        print(f"Linear velocity: {linear_velocity:.2f} m")
+            
+
         # Publish the vehicle command
         vehicle_cmd = VehicleCmd()
         vehicle_cmd.ctrl_cmd.steering_angle = steering_angle
-        vehicle_cmd.ctrl_cmd.linear_velocity = 10.0
+        vehicle_cmd.ctrl_cmd.linear_velocity = linear_velocity
         vehicle_cmd.header.stamp = msg.header.stamp
         vehicle_cmd.header.frame_id = "base_link"
         self.vehicle_cmd_publisher.publish(vehicle_cmd)
