31c517366673b72983b4d434e25120201900b970
Alex Korvyakov <djkp9ik@gmail.com>
2025-07-30 13:04:31 +0300

    Distance from path start

--- nodes/control/pure_pursuit_follower.py
+++ nodes/control/pure_pursuit_follower.py
@@ -4,11 +4,14 @@ import rospy
 from autoware_mini.msg import Path
 from geometry_msgs.msg import PoseStamped
 from autoware_mini.msg import VehicleCmd
+from shapely.geometry import LineString, Point
+from shapely import prepare, distance
 
 class PurePursuitFollower:
     def __init__(self):
 
         # Parameters
+        self.path_linstring = None
 
         # Publishers
         self.vehicle_cmd_publisher = rospy.Publisher('/control/vehicle_cmd', VehicleCmd, queue_size=10)
@@ -20,10 +23,20 @@ class PurePursuitFollower:
         
 
     def path_callback(self, msg):
-        # TODO
-        pass
+        # convert waypoints to shapely linestring
+        path_linestring = LineString([(w.position.x, w.position.y) for w in msg.waypoints])
+        # prepare path - creates spatial tree, making the spatial queries more efficient
+        prepare(path_linestring)
+        self.path_linstring = path_linestring
 
     def current_pose_callback(self, msg):
+        current_pose = Point([msg.pose.position.x, msg.pose.position.y])
+        if self.path_linstring is None:
+            rospy.logwarn("Path not received yet.")
+            return
+        d_ego_from_path_start = self.path_linstring.project(current_pose)
+        print(f"Distance from path start: {d_ego_from_path_start:.2f} m")
+
         # Test publish
         vehicle_cmd = VehicleCmd()
         vehicle_cmd.ctrl_cmd.steering_angle = 0.2
