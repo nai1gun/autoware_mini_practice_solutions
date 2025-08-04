bee9fb48849b23217bc825ebcfb8b060f4d781ca
Alex Korvyakov <djkp9ik@gmail.com>
2025-08-04 15:32:00 +0300

    Cluster detector

---
+++ nodes/detection/cluster_detector.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import rospy
+import numpy as np
+
+from shapely import MultiPoint
+from tf2_ros import TransformListener, Buffer, TransformException
+from numpy.lib.recfunctions import structured_to_unstructured
+from ros_numpy import numpify, msgify
+
+from sensor_msgs.msg import PointCloud2
+from autoware_mini.msg import DetectedObjectArray, DetectedObject
+from std_msgs.msg import ColorRGBA, Header
+from geometry_msgs.msg import Point32
+
+
+BLUE80P = ColorRGBA(0.0, 0.0, 1.0, 0.8)
+
+class ClusterDetector:
+    def __init__(self):
+        self.min_cluster_size = rospy.get_param('~min_cluster_size')
+        self.output_frame = rospy.get_param('/detection/output_frame')
+        self.transform_timeout = rospy.get_param('~transform_timeout')
+
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer)
+
+        self.objects_pub = rospy.Publisher('detected_objects', DetectedObjectArray, queue_size=1, tcp_nodelay=True)
+        rospy.Subscriber('points_clustered', PointCloud2, self.cluster_callback, queue_size=1, buff_size=2**24, tcp_nodelay=True)
+
+        rospy.loginfo("%s - initialized", rospy.get_name())
+
+
+    def cluster_callback(self, msg):
+        data = numpify(msg)
+        points = structured_to_unstructured(data[['x', 'y', 'z']], dtype=np.float32)
+        print("Points shape before transform:", points.shape)
+        print("first row:", points[0])
+        if msg.header.frame_id != self.output_frame:
+            try:
+                transform = self.tf_buffer.lookup_transform(self.output_frame, msg.header.frame_id, msg.header.stamp, rospy.Duration(self.transform_timeout))
+            except (TransformException, rospy.ROSTimeMovedBackwardsException) as e:
+                rospy.logwarn("%s - %s", rospy.get_name(), e)
+                return
+            tf_matrix = numpify(transform.transform).astype(np.float32)
+            # make copy of points
+            points = points.copy()
+            # add homogeneous coordinate
+            points = np.hstack((points, np.ones((points.shape[0], 1), dtype=points.dtype)))
+            # transform points to target frame
+            points = points.dot(tf_matrix.T)
+            print("Transformed points shape:", points.shape)
+            print("Transformed first row:", points[0])
+            print("tf_matrix:", tf_matrix)                                           
+            
+
+    def run(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    rospy.init_node('cluster_detector', log_level=rospy.INFO)
+    node = ClusterDetector()
+    node.run()
