99f48ecbfedc9a261bab58c015536664ae2deca8
Alex Korvyakov <djkp9ik@gmail.com>
2025-08-04 13:53:29 +0300

    Add clustering

--- nodes/detection/points_clusterer.py
+++ nodes/detection/points_clusterer.py
@@ -5,13 +5,17 @@ import numpy as np
 from sensor_msgs.msg import PointCloud2
 from numpy.lib.recfunctions import structured_to_unstructured, unstructured_to_structured
 from ros_numpy import numpify, msgify
+from sklearn.cluster import DBSCAN
 
 class PointsClusterer:
     def __init__(self):
 
         # Parameters
-        # self.cluster_distance_threshold = rospy.get_param('~cluster_distance_threshold', 0.5)
+        self.cluster_epsilon = rospy.get_param('~/detection/lidar/points_clusterer/cluster_epsilon')
+        self.cluster_min_size = rospy.get_param('~/detection/lidar/points_clusterer/cluster_min_size')
 
+        # Initialize the DBSCAN clusterer
+        self.clusterer = DBSCAN(eps=self.cluster_epsilon, min_samples=self.cluster_min_size)
         # Publishers 
 
         # Subscribers
@@ -22,6 +26,13 @@ class PointsClusterer:
         points = structured_to_unstructured(data[['x', 'y', 'z']], dtype=np.float32)
         rospy.loginfo("Points shape %d %d", points.shape[0], points.shape[1])
 
+        # Perform clustering
+        labels = self.clusterer.fit_predict(points)
+        rospy.loginfo("Labels shape: %s", labels.shape)
+
+        # Assert that the number of labels matches the number of points
+        if points.shape[0] != labels.shape[0]:
+            raise AssertionError(f"Number of points ({points.shape[0]}) does not match number of labels ({labels.shape[0]})")
 
     def run(self):
         # Main loop for clustering points
